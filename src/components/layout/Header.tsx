c0f8da8d2d410b867ee5bccb7462dfd991768e14
Muthana Torsson <muthana.tursson@gmail.com>
2026-07-06 19:09:40 +0300

    Configure Tailwind and project structure

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className="bg-blue-600 text-white p-5 shadow-md">
+            <div className="max-w-7xl mx-auto">
+                <h1 className="text-2xl font-bold">LITE.SHOP</h1>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
